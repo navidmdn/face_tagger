a38ff8d642211e21d0027b2bfdf94339b75179e4
Navid Madani <navid.madani@tap30.net>
2020-03-09 22:38:12 +0330

    remove .idea

--- src/application/face_handler.py
+++ src/application/face_handler.py
@@ -7,7 +7,6 @@ def get_face_locations(img):
     locs = []
     for location in face_locations:
         top, right, bottom, left = location
-        print(location)
         locs.append([
             top * img.shape[0] / img.digest_y,
             right * img.shape[1] / img.digest_x,
@@ -19,7 +18,6 @@ def get_face_locations(img):
 
 def get_face_at_location(img, loc, margin=30):
     top, right, bottom, left = loc
-    print(loc)
     result = img.arr[
              max(int(top) - margin, 0):int(bottom) + margin,
              max(int(left) - margin, 0):int(right) + margin]
